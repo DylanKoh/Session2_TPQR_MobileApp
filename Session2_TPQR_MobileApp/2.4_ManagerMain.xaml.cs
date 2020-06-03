1cc5b593559af564d4546ebfb07b03b59e24b6bc
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-06-03 20:31:48 +0800

    2.4 Functionality Done

---
+++ Session2_TPQR_MobileApp/2.4_ManagerMain.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Session2_TPQR_MobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ManagerMain : ContentPage
+    {
+        public ManagerMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnViewPackages_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ViewPackages());
+        }
+
+        private async void btnAddPackages_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddPackages());
+        }
+
+        private async void btnApproveBookings_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ApproveBookings());
+        }
+
+        private async void btnViewSummary_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ViewSummary());
+        }
+    }
+}
---
+++ Session2_TPQR_MobileApp/ViewPackages.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Session2_TPQR_MobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewPackages : ContentPage
+    {
+        public ViewPackages()
+        {
+            InitializeComponent();
+        }
+    }
+}
